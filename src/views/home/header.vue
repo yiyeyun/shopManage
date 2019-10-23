6dbae9c8cdebca22f642f4a0de3b007c3288abae
zhaojianbo <18257346778@163.com>
2019-10-23 23:43:50 +0800

    fds

---
+++ src/views/home/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+      <div>tuabi</div>
+      <div>tuichu</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="less">
+    .box{
+        display: flex;
+        justify-content: space-between;
+        padding: 0 30px;
+        align-items: center;
+        height: 50px;
+        background: #fff;
+        box-shadow: 0 1px 4px rgba(0,21,41,.08);
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,68 @@
 <template>
-  <div class="box">home</div>
+  <div class="box">
+    <el-menu
+      default-active="1-1"
+      class="el-menu-vertical-demo"
+      background-color="#2d3a4b"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      :unique-opened="true"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-goods" />
+          <span>商品管理</span>
+        </template>
+        <el-menu-item index="1-1">商品列表</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-menu" />
+          <span>提货券管理</span>
+        </template>
+        <el-menu-item index="2-1">提货券管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-tickets" />
+          <span>订单管理</span>
+        </template>
+        <el-menu-item index="3-1">订单管理</el-menu-item>
+      </el-submenu>
+    </el-menu>
+    <div class="flex-1 flex flex-column">
+        <top class="bottom-line"></top>
+        <div class="flex-1">
+            <router-view />
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import top from './header'
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: { top },
+  methods: {
+    handleOpen(e) {
+      console.log(1, e)
+    },
+    handleClose(e) {
+      console.log(2, e)
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.box{
+    height: 100%;
+    display: flex;
+}
+    /deep/ .el-menu{
+        width: 200px;
+    }
 </style>
